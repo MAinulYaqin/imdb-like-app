12c3ee25517edb0ffb99cc1e3378fcc933cdd1b1
MAinulYaqin <ainul.y9b@gmail.com>
2018-06-14 12:23:25 +0700

    styling movie item

--- src/components/main/movies/MovieListItem.js
+++ src/components/main/movies/MovieListItem.js
@@ -1,14 +1,28 @@
 import React from 'react';
+import './MovieListItem.css';
 
 const MovieListItem = ({e}) => {
-    let {title, poster_path} = e;
+    let {title, poster_path, release_date, vote_average} = e;
     let imgUrl = `https://image.tmdb.org/t/p/w342/${poster_path}`;
+    let year = release_date.substring(0, 4)
 
     return (
-        <li>
+        <li className="movie-item">
             <img src={imgUrl} alt={title} />
-            <span>{title}</span>
-        </li>
+            <div className="movie-description">
+                <h2>{title}</h2>
+                <section className="movie-details">
+                    <div className="movie-year">
+                        <span className="title">Year</span>
+                        <span>{year}</span>
+                    </div>
+                    <div className="movie-rating">
+                        <span className="title">Rating</span>
+                        <span>{vote_average}</span>
+                    </div>
+                </section>
+            </div>
+    </li>
     )
 }
 
